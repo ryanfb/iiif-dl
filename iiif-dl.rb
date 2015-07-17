607c6d6e0fb64adb590d70311a7a7624c43909b1
Ryan Baumann <ryan.baumann@gmail.com>
2015-07-17 06:30:13 -0400

    Use tempfile library for tempfile construction

--- iiif-dl.rb
+++ iiif-dl.rb
@@ -2,6 +2,7 @@
 
 require 'json'
 require 'fileutils'
+require 'tempfile'
 
 MAX_TILE_WIDTH = 1000
 MAX_TILE_HEIGHT = 1000
@@ -14,7 +15,7 @@ begin
 rescue
 end
 
-metadata_prefix = "#{iiif_manifest['label']}#{manifest_id}"
+metadata_prefix = "#{iiif_manifest['label']}#{manifest_id}".tr(' ','_')
 current_sequence = 0
 iiif_manifest['sequences'].each do |sequence|
   $stderr.puts "Downloading #{sequence['canvases'].length} canvases"
@@ -32,24 +33,28 @@ iiif_manifest['sequences'].each do |sequence|
       final_filename = "#{metadata_prefix} #{current_sequence} #{current_canvas} #{canvas['label']} #{current_image}.jpg".tr(' ','_')
       $stderr.puts "Downloading and assembling #{final_filename}"
       tile = 0
-      filenames = []
-      for y in 0..(y_tiles - 1)
-        for x in 0..(x_tiles - 1)
-          x_offset = MAX_TILE_WIDTH * x
-          y_offset = MAX_TILE_HEIGHT * y
-          x_width = (x_offset + MAX_TILE_WIDTH) > width ? width - x_offset : MAX_TILE_WIDTH
-          y_width = (y_offset + MAX_TILE_HEIGHT) > height ? height - y_offset : MAX_TILE_HEIGHT
-          filename = "#{tile}.jpg"
-          filenames << filename
-          iiif_tile = "#{x_offset},#{y_offset},#{x_width},#{y_width}"
-          url = "#{image['resource']['service']['@id']}/#{iiif_tile}/full/0/default.jpg"
-          $stderr.puts "Downloading tile #{iiif_tile}"
-          `wget -q -O #{filename} #{url}`
-          tile += 1
+      tempfiles = []
+      begin
+        for y in 0..(y_tiles - 1)
+          for x in 0..(x_tiles - 1)
+            x_offset = MAX_TILE_WIDTH * x
+            y_offset = MAX_TILE_HEIGHT * y
+            x_width = (x_offset + MAX_TILE_WIDTH) > width ? width - x_offset : MAX_TILE_WIDTH
+            y_width = (y_offset + MAX_TILE_HEIGHT) > height ? height - y_offset : MAX_TILE_HEIGHT
+            tempfile = Tempfile.new(["#{metadata_prefix}_#{tile}_",'.jpg'])
+            tempfile.close
+            tempfiles << tempfile
+            iiif_tile = "#{x_offset},#{y_offset},#{x_width},#{y_width}"
+            url = "#{image['resource']['service']['@id']}/#{iiif_tile}/full/0/default.jpg"
+            $stderr.puts "Downloading tile #{iiif_tile}"
+            `wget -q -O #{tempfile.path} #{url}`
+            tile += 1
+          end
         end
+        `montage -mode concatenate -tile #{x_tiles}x#{y_tiles} #{tempfiles.map{|t| t.path}.join(' ')} #{final_filename}`
+      ensure
+        tempfiles.each{|t| t.unlink}
       end
-      `montage -mode concatenate -tile #{x_tiles}x#{y_tiles} #{filenames.join(' ')} #{final_filename}`
-      FileUtils.rm(filenames)
       current_image += 1
     end # image loop
     current_canvas += 1
