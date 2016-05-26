47b6065009c440672a521730c5ff380a48fdfaf5
Ryan Baumann <ryan.baumann@gmail.com>
2016-05-26 11:13:52 -0400

    Respect tile sizes from info.json (fixes #5)

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 source 'https://rubygems.org'
 gem 'robotex'
+gem 'open_uri_redirections'
--- iiif-dl.rb
+++ iiif-dl.rb
@@ -5,9 +5,11 @@ require 'fileutils'
 require 'tempfile'
 require 'robotex'
 require 'uri'
+require 'open-uri'
+require 'open_uri_redirections'
 
-MAX_TILE_WIDTH = 1000
-MAX_TILE_HEIGHT = 1000
+DEFAULT_TILE_WIDTH = 1000
+DEFAULT_TILE_HEIGHT = 1000
 DEFAULT_EXTENSION = 'jpg'
 USER_AGENT = 'iiif-dl'
 DEFAULT_DELAY = 1
@@ -33,8 +35,36 @@ iiif_manifest['sequences'].each do |sequence|
       $stderr.puts "#{image['resource']['width']} x #{image['resource']['height']}"
       width = image['resource']['width'].to_i
       height = image['resource']['height'].to_i
-      x_tiles = (width / MAX_TILE_WIDTH.to_f).ceil
-      y_tiles = (height / MAX_TILE_HEIGHT.to_f).ceil
+      identifier = image['resource']['service']['@id'].chomp('/')
+      max_tile_width = DEFAULT_TILE_WIDTH
+      max_tile_height = DEFAULT_TILE_HEIGHT
+      
+      begin
+        info_json_url = URI.escape("#{identifier}/info.json")
+        if robotex.allowed?(info_json_url)
+          info_json = JSON.parse(open(info_json_url, "User-Agent" => USER_AGENT, :allow_redirections => :all).read)
+          if info_json['tiles'] # IIIF 2.0
+            max_tile_width = info_json['tiles'][0]['width']
+            if info_json['tiles'][0]['height']
+              max_tile_height = info_json['tiles'][0]['height']
+            else
+              max_tile_height = max_tile_width
+            end
+          else # IIIF 1.1
+            if info_json['tile_width']
+              max_tile_width = info_json['tile_width']
+            end
+            if info_json['tile_height']
+              max_tile_height = info_json['tile_height']
+            end
+          end
+        end
+      rescue Exception => e
+        $stderr.puts "Error parsing info.json (#{e.message}), using default tile size"
+      end
+
+      x_tiles = (width / max_tile_width.to_f).ceil
+      y_tiles = (height / max_tile_height.to_f).ceil
       $stderr.puts "#{x_tiles} x #{y_tiles} tiles"
       final_filename = "#{metadata_prefix} #{current_sequence} #{current_canvas} #{canvas['label']} #{current_image}.jpg".gsub(/[^-.a-zA-Z0-9_]/,'_')
       $stderr.puts "Downloading and assembling #{final_filename}"
@@ -43,13 +73,13 @@ iiif_manifest['sequences'].each do |sequence|
       begin
         for y in 0..(y_tiles - 1)
           for x in 0..(x_tiles - 1)
-            x_offset = MAX_TILE_WIDTH * x
-            y_offset = MAX_TILE_HEIGHT * y
-            x_width = (x_offset + MAX_TILE_WIDTH) > width ? width - x_offset : MAX_TILE_WIDTH
-            y_width = (y_offset + MAX_TILE_HEIGHT) > height ? height - y_offset : MAX_TILE_HEIGHT
+            x_offset = max_tile_width * x
+            y_offset = max_tile_height * y
+            x_width = (x_offset + max_tile_width) > width ? width - x_offset : max_tile_width
+            y_width = (y_offset + max_tile_height) > height ? height - y_offset : max_tile_height
             iiif_tile = "#{x_offset},#{y_offset},#{x_width},#{y_width}"
             quality = image['resource']['service']['@context'] =~ /iiif.io\/api\/image\/2/ ? 'default' : 'native'
-            url = URI.escape("#{image['resource']['service']['@id'].chomp('/')}/#{iiif_tile}/full/0/#{quality}.#{DEFAULT_EXTENSION}")
+            url = URI.escape("#{identifier}/#{iiif_tile}/full/0/#{quality}.#{DEFAULT_EXTENSION}")
             if robotex.allowed?(url)
               $stderr.puts "Downloading tile #{iiif_tile}"
               tempfile = Tempfile.new(["#{metadata_prefix}_#{tile}_",'.jpg'])
