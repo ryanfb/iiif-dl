671f94fc8069a7f06b2fe73a624e5c8b6dc75a05
Ryan Baumann <ryan.baumann@gmail.com>
2018-07-24 12:23:34 -0400

    Add command-line options for single-identifiers and forced-tiling (fixes #6)

--- README.md
+++ README.md
@@ -16,7 +16,11 @@ See also: [dzi-dl](https://github.com/ryanfb/dzi-dl/)
 ## Usage
 
     bundle install
-    bundle exec ./iiif-dl.rb iiif-manifest.json
+    bundle exec ./iiif-dl.rb --help
+    Usage: iiif-dl.rb [options] [iiif-manifest.json]
+        -f, --force-tiling               Don't attempt full-size downloads without tiling
+        -i, --identifier IDENTIFIER      Download single IIIF identifier IDENTIFIER instead of image sequence from manifest JSON
+        -h, --help                       Display this screen
 
 You can download an IIIF manifest from e.g. the [Digital Bodleian](http://digital.bodleian.ox.ac.uk/):
 
@@ -25,3 +29,5 @@ You can download an IIIF manifest from e.g. the [Digital Bodleian](http://digita
 Alternately, if you have [PhantomJS](http://phantomjs.org/) installed, you can use `jsonreqs.js` to list all URLs ending in `.json` requested by a given webpage URL:
 
     phantomjs jsonreqs.js 'http://example.com/viewer.asp?manuscript=shelfmark'
+
+In single-identifier mode, you pass the URL of an IIIF identifier, i.e. what `/info.json` would be appended to in order to make [an Image Information Request](https://iiif.io/api/image/2.1/#image-information-request).
--- iiif-dl.rb
+++ iiif-dl.rb
@@ -8,6 +8,7 @@ require 'uri'
 require 'open-uri'
 require 'open_uri_redirections'
 require 'dimensions'
+require 'optparse'
 
 DEFAULT_TILE_WIDTH = 1024
 DEFAULT_TILE_HEIGHT = 1024
@@ -16,11 +17,11 @@ USER_AGENT = 'iiif-dl'
 DEFAULT_DELAY = 1
 ROBOTEX = Robotex.new(USER_AGENT)
 
-def download_identifier(identifier, final_filename = nil, attempt_full = true, v2 = false, width = nil, height = nil)
+def download_identifier(identifier, force_tiling = false, final_filename = nil, width = nil, height = nil)
   final_filename ||= identifier.split('/').last
   max_tile_width = DEFAULT_TILE_WIDTH
   max_tile_height = DEFAULT_TILE_HEIGHT
-  quality = v2 ? 'default' : 'native'
+  v2 = nil
   
   begin
     info_json_url = URI.escape("#{identifier}/info.json")
@@ -28,6 +29,7 @@ def download_identifier(identifier, final_filename = nil, attempt_full = true, v
     if ROBOTEX.allowed?(info_json_url)
       info_json = JSON.parse(open(info_json_url, "User-Agent" => USER_AGENT, :allow_redirections => :all).read)
       if info_json['tiles'] # IIIF 2.0
+        v2 = true
         max_tile_width = info_json['tiles'][0]['width']
         if info_json['tiles'][0]['height']
           max_tile_height = info_json['tiles'][0]['height']
@@ -35,6 +37,7 @@ def download_identifier(identifier, final_filename = nil, attempt_full = true, v
           max_tile_height = max_tile_width
         end
       else # IIIF 1.1
+        v2 = false
         if info_json['tile_width']
           max_tile_width = info_json['tile_width']
         end
@@ -56,7 +59,8 @@ def download_identifier(identifier, final_filename = nil, attempt_full = true, v
     $stderr.puts "Error parsing info.json (#{e.message}), using default tile size"
   end
 
-  if attempt_full
+  quality = v2 ? 'default' : 'native'
+  unless force_tiling
     [ URI.escape("#{identifier}/0,0,#{width},#{height}/full/0/#{quality}.#{DEFAULT_EXTENSION}"),
       URI.escape("#{identifier}/full/full/0/#{quality}.#{DEFAULT_EXTENSION}") ].each do |url|
       $stderr.puts "Attempting full-size download without stitching: #{url}"
@@ -77,7 +81,7 @@ def download_identifier(identifier, final_filename = nil, attempt_full = true, v
 
   x_tiles = (width / max_tile_width.to_f).ceil
   y_tiles = (height / max_tile_height.to_f).ceil
-  $stderr.puts "#{x_tiles} x #{y_tiles} tiles"
+  $stderr.puts "#{x_tiles} x #{y_tiles} = #{x_tiles * y_tiles} tiles"
   $stderr.puts "Downloading and assembling: #{final_filename}.jpg"
   tile = 0
   tempfiles = []
@@ -115,37 +119,60 @@ def download_identifier(identifier, final_filename = nil, attempt_full = true, v
   end
 end
 
-if __FILE__ == $PROGRAM_NAME
-  iiif_manifest = JSON.parse(ARGF.read)
-
-  manifest_id = ''
-  begin
-   manifest_id = " #{iiif_manifest['metadata'].select{|m| m['label'] == 'Id'}.first['value']}"
-  rescue
+if File.basename(__FILE__) == File.basename($PROGRAM_NAME)
+  options = {}
+  optparse = OptionParser.new do |opts|
+    opts.banner = "Usage: iiif-dl.rb [options] [iiif-manifest.json]"
+    options[:force_tiling] = false
+    opts.on('-f','--force-tiling',"Don't attempt full-size downloads without tiling") do
+      options[:force_tiling] = true
+    end
+    options[:identifier_download] = nil
+    opts.on('-i','--identifier IDENTIFIER',"Download single IIIF identifier IDENTIFIER instead of image sequence from manifest JSON") do |identifier|
+      options[:identifier_download] = identifier
+    end
+    opts.on('-h','--help','Display this screen') do
+      puts opts
+      exit
+    end
   end
+  optparse.parse!
 
-  metadata_prefix = "#{iiif_manifest['label']}#{manifest_id}".gsub(/[^-.a-zA-Z0-9_]/,'_')
-  current_sequence = 0
-  iiif_manifest['sequences'].each do |sequence|
-    $stderr.puts "Downloading #{sequence['canvases'].length} canvases"
-    current_canvas = 0
-    sequence['canvases'].each do |canvas|
-      $stderr.puts canvas['label']
-      current_image = 0
-      canvas['images'].each do |image|
-        $stderr.puts "#{image['resource']['width']} x #{image['resource']['height']}"
-        width = image['resource']['width'].to_i
-        height = image['resource']['height'].to_i
-        identifier = URI.unescape(image['resource']['service']['@id'].chomp('/'))
-        $stderr.puts "Got identifier: #{identifier}"
-        $stderr.puts "From: #{image['resource']['service']['@id']}"
-        final_filename = "#{metadata_prefix} #{current_sequence} #{current_canvas} #{canvas['label']} #{current_image}".gsub(/[^-.a-zA-Z0-9_]/,'_')
-        v2 = image['resource']['service']['@context'] =~ /iiif.io\/api\/image\/2/
-        download_identifier(identifier, final_filename, true, v2, width, height)
-        current_image += 1
-      end # image loop
-      current_canvas += 1
-    end # canvas loop
-    current_sequence += 1
-  end # sequence loop
+  if options[:identifier_download]
+    download_identifier(options[:identifier_download], options[:force_tiling])
+  else
+    $stderr.puts "Parsing manifest JSON..."
+    iiif_manifest = JSON.parse(ARGF.read)
+
+    manifest_id = ''
+    begin
+     manifest_id = " #{iiif_manifest['metadata'].select{|m| m['label'] == 'Id'}.first['value']}"
+    rescue
+    end
+
+    metadata_prefix = "#{iiif_manifest['label']}#{manifest_id}".gsub(/[^-.a-zA-Z0-9_]/,'_')
+    current_sequence = 0
+    iiif_manifest['sequences'].each do |sequence|
+      $stderr.puts "Downloading #{sequence['canvases'].length} canvases"
+      current_canvas = 0
+      sequence['canvases'].each do |canvas|
+        $stderr.puts canvas['label']
+        current_image = 0
+        canvas['images'].each do |image|
+          $stderr.puts "#{image['resource']['width']} x #{image['resource']['height']}"
+          width = image['resource']['width'].to_i
+          height = image['resource']['height'].to_i
+          identifier = URI.unescape(image['resource']['service']['@id'].chomp('/'))
+          $stderr.puts "Got identifier: #{identifier}"
+          $stderr.puts "From: #{image['resource']['service']['@id']}"
+          final_filename = "#{metadata_prefix} #{current_sequence} #{current_canvas} #{canvas['label']} #{current_image}".gsub(/[^-.a-zA-Z0-9_]/,'_')
+          v2 = image['resource']['service']['@context'] =~ /iiif.io\/api\/image\/2/
+          download_identifier(identifier, options[:force_tiling], final_filename, width, height)
+          current_image += 1
+        end # image loop
+        current_canvas += 1
+      end # canvas loop
+      current_sequence += 1
+    end # sequence loop
+  end
 end
